724e8b7e5940eb22f6dd67f768cf518e9d789a8a
Shubham Kumar <sourabhzyyron34@gmail.com>
2026-05-06 13:53:24 +0530

    Initial Commits

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl  text-blue-600 bg-gray-100 p-4 rounded-lg shadow-md">
+        Welcome to the Dashboard page
+      </h1>
+    </>
+  );
+}
+
+export default App;
